f68fafdc998acaa950ebdf7a82d499706f1550f4
Enjie <angelmuhaeni@gmail.com>
2026-06-10 17:58:46 +0700

    fix: update site metadata and favicon path in layout

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // Allow loading images from Unsplash
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
